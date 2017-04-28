2163c957ad52ee07dab3876b318d736832ecc3b7
Marios Kogias <marios.kogias@epfl.ch>
2017-04-28 16:56:48 +0200

    Use mempools for reading from storage

--- dp/core/dummy_dev.c
+++ dp/core/dummy_dev.c
@@ -4,6 +4,9 @@
 #include <ix/page.h>
 #include <ix/errno.h>
 #include <ix/cpu.h>
+#include <ix/mbuf.h>
+
+#include <assert.h>
 
 #define STORAGE_PAGE_NR 8
 
@@ -30,7 +33,19 @@ int dummy_dev_write(void *payload, uint64_t lba, uint64_t lba_count)
 
 void dummy_dev_read(uint64_t lba, uint64_t lba_count)
 {
-	char tmp[512];
-	memcpy(tmp, &percpu_get(dummy_dev)[lba * LBA_SIZE], lba_count * LBA_SIZE);
-	printf("I read %s\n", tmp);
+	assert(lba_count <= MBUF_DATA_LEN / LBA_SIZE);
+
+	struct mbuf *read_mbuf = mbuf_alloc_local();
+	char *data = mbuf_mtod(read_mbuf, char *);
+	memcpy(data, &percpu_get(dummy_dev)[lba * LBA_SIZE], lba_count * LBA_SIZE);
+	printf("I read %s\n", data);
+
+	// Submit to USYS arr
+	mbuf_free((void *)data);
+}
+
+void dummy_dev_read_done(void *data)
+{
+	struct mbuf *b = (struct mbuf *)((uintptr_t) data - MBUF_HEADER_LEN);
+	mbuf_free(b);
 }
