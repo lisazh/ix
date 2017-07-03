7f8a16b555222800cd46a13bebf897654b804377
Lisa Zhou <lzhou@icnals16.epfl.ch>
2017-07-03 19:54:35 +0200

    automated tweaks...

--- bench.py
+++ bench.py
@@ -3,20 +3,21 @@
 
 
 import os
+import signal
 import subprocess
 import time
-import numpy 
-import matlibplot.pyplot as plot
+#import numpy ##TODO find module 
+#import matlibplot.pyplot as plot
 
 
 MODES = ['0', '1']
 BENCH_PARAMS = []
 RESULT_DIR = ""
 #BASE_DIR = '/home/'
-IX_CMD = BASE_DIR + 'dp/ix'
-BENCH_CMD = BASE_DIR + 'apps/iobench'
+IX_CMD = 'dp/ix'
+BENCH_CMD = 'apps/iobench'
 
-WAIT = 5
+WAIT = 20
 
 """
 """
@@ -38,7 +39,7 @@ def benchmark_singles(mode, cwd):
 		#kill 	
 
 def benchmark_batches(mode, cwd):
-
+	pass
 
 def run_benchmark(m):
 	cwd = os.getcwd()
@@ -51,21 +52,40 @@ def main():
 
 	##for m in MODES:
 		##run_benchmark(m)
-
+	#os.setpgrp()
+	outfile = os.open("out.ix", os.O_CREAT | os.O_TRUNC |os.O_WRONLY)
+	cwd = os.getcwd()
 	proc = subprocess.Popen(
-		['sudo', cwd + IX_CMD, '--', cwd + BENCH_CMD, mode, '1', arg], \
-		stdout=PIPE)
+		['sudo', cwd + '/' +  IX_CMD, '--', cwd + '/' + BENCH_CMD, '1', '1', '10'], \
+		stdout=outfile)
 
-	#output = proc.communicate()[0]
-
+	#out = proc.communicate()[0]
+	#print out
 
 	for i in range(WAIT):
-		if proc.poll() is not None:
-			break
 		time.sleep(1)
-
+	"""
+		if proc.poll() is not None:
+			print "Process exited\n"
+			break
+		else: 
+			print "Waiting for process exit\n"
+			time.sleep(1)
+	"""
+	
 	#kill process anyway..
-	proc.kill()
+	print "Process group {} and PID {}".format(os.getpgrp(), proc.pid) 
+	##os.killpg(0, signal.SIGKILL)
+	p = subprocess.Popen(['sudo', 'killall', 'ix'])
+	
+	time.sleep(1)	
+	os.close(outfile)
+	#check it was killed..
+	p1 = subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE)
+	p2 = subprocess.Popen(['grep', 'ix'], stdin=p1.stdout, stdout=subprocess.PIPE)
+	p1.stdout.close()
+	output = p2.communicate()[0]
+	print output
 
 if __name__=="__main__":
 	main()
