38ec0c8c28308957f5546c98721a9c3659dda622
Lisa Zhou <lisa.zhou@epfl.ch>
2017-07-04 17:47:03 +0200

    bench script improvements

--- bench.py
+++ bench.py
@@ -1,12 +1,13 @@
 """
 """
 
-
+import sys
 import os
 import signal
 import subprocess
 import time
 import mmap
+import fnmatch
 #import numpy ##TODO find module 
 #import matlibplot.pyplot as plot
 
@@ -19,7 +20,7 @@ RESULT_DIR = ""
 IX_CMD = 'dp/ix'
 BENCH_CMD = 'apps/iobench'
 
-WAIT_BASE = 10 #scientifically determined minimum wait time for IX startup...
+WAIT_BASE = 5 #scientifically determined minimum wait time for IX startup...
 
 """
 """
@@ -30,28 +31,65 @@ def graph_results():
 def benchmark_singles(mode, cwd):
 
 	for arg in IO_SZS:
+		outfile = open('out.ix', 'w+')
+		mfile = mmap.mmap(outfile.fileno(), 0)
 		proc = subprocess.Popen(
-			['sudo', cwd + IX_CMD, '--', cwd + BENCH_CMD, mode, '1', arg], \
-			stdout=PIPE)
+			['sudo', cwd + '/' +  IX_CMD, '--', cwd + '/' + BENCH_CMD, mode, '1', arg], \
+			stdout=outfile)
 
-		for i in range(WAIT):
-			time.sleep(1)
+		time.sleep(WAIT_BASE)
 
+		for i in range(WAIT_BASE * 10): #TODO: replace with some value based on size of benchmark..
+			if mfile.find('clean up complete') == -1:
+				time.sleep(1)
+			else: 
+				print 'benchmark completed\n'
+				break
+	
+		p = subprocess.Popen(['sudo', 'killall', 'ix'])
+	
+		time.sleep(1)	
+
+		#TODO grep and do smthg with index building time...
+
+		mfile.close()
+		outfile.close()
+	
+	#check it was killed..
+		p1 = subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE)
+		p2 = subprocess.Popen(['grep', 'ix'], stdin=p1.stdout, stdout=subprocess.PIPE)
+		p1.stdout.close()
+		output = p2.communicate()[0]
+		print output
+
+	##TODO move files somewhere
+
+	resultdir = os.path.join(cwd, 'resultsraw')
+	os.mkdir(resultsdir)
+
+	
+	for f in os.listdir('.'):
+    	if fnmatch.fnmatch(f, 'results_*.ix'):
+        	os.rename(os.path.join(cwd, f), os.path.join(resultsdir, f))
 		
-		#kill 	
-
 def run_benchmark(m):
 	cwd = os.getcwd()
-	benchmark_singles(m, cwd)
-	benchmark_batches(m, cwd)
-
+	if sys.argv[1] == 's':
+		print "Running singles benchmark\n"
+		benchmark_singles(m, cwd)
+	else if sys.argv[1] == 'b':
+		print "Running batches benchmark\n"
+		benchmark_batches(m, cwd)
+	else:
+		print "No benchmark type specified..exiting.\n"
 
 
 def main():
 
-	##for m in MODES:
-		##run_benchmark(m)
-	#os.setpgrp()
+	for m in MODES:
+		run_benchmark(m)
+
+	"""
 	outfile = open('out.ix', 'w+')
 	mfile = mmap.mmap(outfile.fileno(), 0)
 	cwd = os.getcwd()
@@ -86,6 +124,8 @@ def main():
 	p1.stdout.close()
 	output = p2.communicate()[0]
 	print output
+
+	"""
 
 if __name__=="__main__":
 	main()
