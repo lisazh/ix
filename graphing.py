5269933c0c891ed88b74e40bc8d2fb35c2f0a332
Lisa Zhou <lisa.zhou@epfl.ch>
2017-07-07 13:17:39 +0100

    last graphs

--- graphing.py
+++ graphing.py
@@ -14,22 +14,25 @@ def compute_iops(perfus, numreq):
 
 
 
-def getavg(fname):
-	darr = np.loadtxt(fname, delimiter=',', usecols=(1,), ndmin=0)
+def getperc(p, fname):
+	darr = np.loadtxt(fname, delimiter=',', usecols=(1,), ndmin=0, skiprows=2)
 	#print "testing {}".format(fname)
 	#print darr
 
-	weights = np.ones(len(darr))
-	weights[0] = 0 #hack to remove first entry..
+	#weights = np.ones(len(darr))
+	#weights[0] = 0 #hack to remove first entry..
 
-	ret = np.average(darr, weights=weights)
+	#ret = np.average(darr, weights=weights)
+	#ret = np.mean(darr)
 	#print "ret is {}".format(ret)
+	altret = np.percentile(darr, p)
+	#print "altret is {}".format(ret)
 
-	return ret
+	return altret
 	
 def getfiodata(fiores):
 	arr = []
-	rg = re.compile('(?:\s*write\:).*(?<=iops\=)\d+(?=, runt)')
+	rg = re.compile('.*(?<=iops\=)\d+(?=, runt)')
 
 	f = open(os.path.join(os.getcwd(), fiores))
 	for line in f:
@@ -48,7 +51,7 @@ def grepdata(fpath):
 	#print "Time was {}".format(m.group(0))
 	return int(m.group(0))
 
-def getbatchdata(sz, dirname):
+def wgetbatchdata(sz, dirname):
 
 	arr = []
 	for root, dirs, fs in os.walk(os.path.join(os.getcwd(), dirname)):
@@ -60,66 +63,119 @@ def getbatchdata(sz, dirname):
 	#print "Times for {0} requests were {1}".format(sz, arr)
 	return compute_iops(np.mean(arr), sz)			
 
+def rgetbatchdata(sz, dirname):
+
+	arr = []
+	for root, dirs, fs in os.walk(os.path.join(os.getcwd(), dirname)):
+		for d in dirs:
+			for fname in os.listdir(os.path.join(root, d)):
+				if fnmatch.fnmatch(fname, "results_r_{}_*.ix".format(str(sz))):
+					arr.append(grepdata(os.path.join(root, d, fname)))
+
+	#print "Times for {0} requests were {1}".format(sz, arr)
+	return compute_iops(np.mean(arr), sz)			
 
 
 
-def getdatal(sz, dirname):
+def getavgdata(sz, dirname):
 
 	rg = re.compile('results_.*(?<=_){}.ix$'.format(sz))
 	fullpath = os.path.join(os.getcwd(), dirname)	
 	for fname in os.listdir(fullpath):
 			if (rg.match(fname)):
 			#if fnmatch.fnmatch(fname, "results_*{}.ix".format(str(sz))):
-				print "Matched file {0} for io size {1}".format(fname, sz)
-				return getavg(os.path.join(fullpath, fname))
+				#print "Matched file {0} for io size {1}".format(fname, sz)
+				return getperc(50, os.path.join(fullpath, fname))
 
-def getindexdata():
+def getnndata(sz, dirname):
+	rg = re.compile('results_.*(?<=_){}.ix$'.format(sz))
+	fullpath = os.path.join(os.getcwd(), dirname)	
+	for fname in os.listdir(fullpath):
+			if (rg.match(fname)):
+			#if fnmatch.fnmatch(fname, "results_*{}.ix".format(str(sz))):
+				#print "Matched file {0} for io size {1}".format(fname, sz)
+				return getperc(99, os.path.join(fullpath, fname))
 
+
+def getindexdata(sz, dirname):
+
+	tmp = []
 	rg = re.compile('(?:DEBUG: index building took )\d+(?= milliseconds)')
+	rf = re.compile('out_{}.ix'.format(str(sz)))
 
+	fullpath = os.path.join(os.getcwd(), dirname)
+	for fname in os.listdir(fullpath):
+		if (rf.match(fname)):
+			f = open(os.path.join(fullpath, fname), 'r')
+			for line in f:
+				if rg.match(line):
+					tmp.append(int(re.search('\d+(?= milliseconds)', line).group()))
+
+	#print "Times for creating index of size {0} were {1}".format(sz, tmp)
+
+	return np.mean(tmp)
 
 
 def graph_iosizes():
 
-	vect = np.vectorize(getdatal)
+	avgvect = np.vectorize(getavgdata)
+	nnvect = np.vectorize(getnndata)
 	writedir = os.path.join(SRESULT_DIR, "writes")
 	readsdir = os.path.join(SRESULT_DIR, "reads")
-	wperf = vect(IO_SZS, writedir)
-	rperf = vect(IO_SZS, readsdir)
+	
+	#wperf = vect(IO_SZS, writedir)
+	#rperf = vect(IO_SZS, readsdir)
 
-	bwperf = vect(IO_BSZS, writedir)
-	brperf = vect(IO_BSZS, readsdir)
+	bwperf_avg = avgvect(IO_BSZS, writedir)
+	brperf_avg = avgvect(IO_BSZS, readsdir)
 
-	print wperf
-	print rperf
+	bwperf_nn = nnvect(IO_BSZS, writedir)
+	brperf_nn = nnvect(IO_BSZS, readsdir)
+
+	#print bwperf_nn
+	#print brperf_nn
 	#TODO different lines.. how label
 	#plot.plot(IO_SZS, wperf, 'bo')
 	#plot.plot(IO_SZS, wperf, 'b', label='Writes')
 	#plot.plot(IO_SZS, rperf, 'go')
 	#plot.plot(IO_SZS, rperf, 'g', label='Reads')
-	plot.plot(IO_BSZS, bwperf, 'yo')
-	plot.plot(IO_BSZS, bwperf, 'y', label="Writes (binary)")
-	plot.plot(IO_BSZS, brperf, 'ro')
-	plot.plot(IO_BSZS, brperf, 'r', label="Reads (binary sizes)")
 
-	plot.xlabel('IO Size (bytes)')
-	plot.ylabel('Average Latency (us)')
-	plot.title('Performance over IO sizes')
+	fig, ax = plot.subplots()
 
-	plot.legend(loc='upper left')
+	ax.plot(IO_BSZS, bwperf_avg, 'y.')
+	ax.plot(IO_BSZS, bwperf_avg, 'y', linestyle="-", label="Writes (average)")
+	ax.plot(IO_BSZS, bwperf_nn, 'y.')
+	ax.plot(IO_BSZS, bwperf_nn, 'y', linestyle="--", label="Writes (99th)")
+
+	ax.plot(IO_BSZS, brperf_avg, 'b.')
+	ax.plot(IO_BSZS, brperf_avg, 'b', linestyle="-", label="Reads (average)")
+	ax.plot(IO_BSZS, brperf_nn, 'b.')
+	ax.plot(IO_BSZS, brperf_nn, 'b', linestyle="--", label="Reads (99th)")
+
+	ax.set_xlabel('IO Size (bytes)')
+	ax.set_ylabel('Latency (us)')
+	ax.set_title('Performance over IO sizes')
+
+	ax.legend(loc='upper left')
 	plot.savefig('perf_iosizes.png')
 
 
 def graph_indexrebuild():
 
-	rebuildtime = [652.0, 646.0, 664.0, 656.0, 651.0, 669.0, 716.0, 749.0, 801.0] 
+	#rebuildtime = [652.0, 646.0, 664.0, 656.0, 651.0, 669.0, 716.0, 749.0, 801.0] 
 	#rebuildtime = [866.0, 861.0, 858.0, 867.0, 860.0, 892.0, 875.0, 865.0, 861.0]
+	indvect = np.vectorize(getindexdata)
 
-	plot.plot(STOR_SZS, rebuildtime)
-	plot.plot(STOR_SZS, rebuildtime, 'bo')
-	plot.xlabel("Index size (number of 512B entries)")
-	plot.ylabel("Time (milliseconds)")
-	plot.title("Index rebuilding")
+	rebuildtime = indvect(STOR_SZS, IRESULT_DIR)
+
+
+	fig, ax = plot.subplots()
+
+	ax.plot(STOR_SZS, rebuildtime)
+	ax.plot(STOR_SZS, rebuildtime, 'bo')
+	ax.set_xlabel("Index size (number of 512B entries)")
+	ax.set_ylabel("Time (milliseconds)")
+	ax.set_title("Index rebuilding")
 
 	plot.savefig('indexrebuild.png')
 
@@ -127,17 +183,17 @@ def graph_indexrebuild():
 def graph_tpoh():
 	pass
 
-def graph_batch():
+def graph_wbatch():
 	
-	vect = np.vectorize(getbatchdata)
+	vect = np.vectorize(wgetbatchdata)
 	res_delay = vect(BT_SZS, BRESULT_DIR)
 	res_nodelay = vect(BT_SZS, NODELAYDIR)
-	res_fio = getfiodata('fio_res.ix')
+	res_fio = getfiodata('wfio_res.ix')
 
 
-	#print res_delay
-	#print res_nodelay
-	#print res_fio
+	print res_delay
+	print res_nodelay
+	print res_fio
 
 	ind = np.arange(1,5)
 	width = 0.25
@@ -150,14 +206,45 @@ def graph_batch():
 	# add some text for labels, title and axes ticks
 	ax.set_ylabel('IOPS')
 	ax.set_xlabel('Batch size (# requests)')
-	ax.set_title('IOPS or smthg')
+	ax.set_title('Write IOPS')
 	ax.set_xticks(ind + width * 3/2)
 	ax.set_xticklabels(BT_SZS) #TODO LOG SCALE
 
 	ax.legend((rects1[0], rects2[0], rects3[0]), ('IX with delay', 'fio on ramdisk', 'IX w/o delay'), loc='upper left')
 
+	plot.savefig('writeiops.png')
 
-	plot.savefig('sampleiops.png')
+def graph_rbatch():
+	
+	vect = np.vectorize(rgetbatchdata)
+	res_delay = vect(BT_SZS, BRESULT_DIR)
+	res_nodelay = vect(BT_SZS, NODELAYDIR)
+	res_fio = getfiodata('rfio_res.ix')
+
+
+	print res_delay
+	print res_nodelay
+	print res_fio
+
+	ind = np.arange(1,5)
+	width = 0.25
+
+	fig, ax = plot.subplots()
+	rects1 = ax.bar(ind, res_delay, width, color='r')
+	rects2 = ax.bar(ind + width, res_fio, width, color='y')
+	rects3 = ax.bar(ind + 2*width, res_nodelay, width, color='g')
+
+	# add some text for labels, title and axes ticks
+	ax.set_ylabel('IOPS')
+	ax.set_xlabel('Batch size (# requests)')
+	ax.set_title('Read IOPS')
+	ax.set_xticks(ind + width * 3/2)
+	ax.set_xticklabels(BT_SZS) #TODO LOG SCALE
+
+	ax.legend((rects1[0], rects2[0], rects3[0]), ('IX with delay', 'fio on ramdisk', 'IX w/o delay'), loc='upper left')
+
+	plot.savefig('readiops.png')
+
 
 
 
@@ -169,9 +256,13 @@ if __name__=="__main__":
 		darr = np.loadtxt(f, delimiter='', usecols = (), ndmin=0)
 	"""
 
+	
 	graph_iosizes()
-	graph_batch()
-	#graph_indexrebuild()
+	graph_indexrebuild()
+
+	graph_rbatch()
+	graph_wbatch()
+
 
 	'''
 	iosizes = [100, 500, 1000]
